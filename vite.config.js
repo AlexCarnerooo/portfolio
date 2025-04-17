f1e266396a7062de9ac3b39e95dfed34899ade01
AlexCarnerooo <alexandrecarnerop@gmail.com>
2025-04-17 15:31:03 +0200

    Fix image handling and favicon for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',  // Asegúrate de que coincida exactamente con el nombre de tu repositorio
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
 }) 
